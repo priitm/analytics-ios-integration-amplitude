03606ffcbded6a4dc53af00fcdeac566cfd2eeea
Priit <priitm@gmail.com>
2021-02-23 17:51:56 -0500

    swift tools version and dependency names

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
   name: "SegmentAmplitude",
+  platforms: [
+    .iOS(.v10)
+  ],
   products: [
     .library(
       name: "SegmentAmplitude",
@@ -10,15 +13,18 @@ let package = Package(
   ],
   dependencies: [
     .package(
+      name: "Amplitude",
       url: "https://github.com/amplitude/Amplitude-iOS",
-      .exact("5.3.0")),
+      .exact("7.3.0")),
     .package(
+      name: "Segment",
       url: "https://github.com/segmentio/analytics-ios",
-      .exact("4.1.3"))
+      .exact("4.1.2"))
   ],
   targets: [
     .target(
       name: "SegmentAmplitude",
+      dependencies: ["Segment", "Amplitude"],
       path: "Pod",
       cSettings: [
         .headerSearchPath("Classes"),
