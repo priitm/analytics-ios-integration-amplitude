71d69be8aa133248e2e9bace468452217cde4155
Priit <priitm@gmail.com>
2021-02-23 15:52:06 -0500

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,23 +3,25 @@ import PackageDescription
 
 let package = Package(
   name: "SegmentAmplitude",
-  platforms: [
-    .iOS(.v12),
-  ],
   products: [
     .library(
       name: "SegmentAmplitude",
       targets: ["SegmentAmplitude"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: "git@github.com:segmentio/analytics-ios.git", from: "3.6.0"),
-    // .package(url: "git@github.com:amplitude/Amplitude-iOS.git", from: "4.0.0"),
+    .package(
+      url: "https://github.com/amplitude/Amplitude-iOS",
+      Version(5, 3, 0)),
+    .package(
+      url: "https://github.com/segmentio/analytics-ios",
+      Version(4, 1, 3))
   ],
   targets: [
     .target(
       name: "SegmentAmplitude",
-      dependencies: []),
-      // dependencies: ["Analytics", "Amplitude"]),
+      path: "Pod",
+      cSettings: [
+      .headerSearchPath("Classes"),
+    ]),
   ]
 )
